0e171d63e75433e4b5966a58abd47ddee5590047
Connor Atkins <connoratkins@hotmail.co.uk>
2022-06-29 05:15:22 +0100

    fixed page transition

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+import { useLayoutEffect } from 'react';
 import { Home, PrivacyPage } from './pages';
+
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
 
 function App() {
   return (
     <Router>
-      <div className='App'>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/privacypolicy' element={<PrivacyPage />}></Route>
-        </Routes>
-      </div>
+      <Wrapper>
+        <div className='App'>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/privacypolicy' element={<PrivacyPage />}></Route>
+          </Routes>
+        </div>
+      </Wrapper>
     </Router>
   );
 }
